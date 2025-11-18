8a03c5b88151e113df4963ff33b1ac62791980be
ono.rikuto <ono.rikuto@dsic.jp>
2025-11-18 10:29:05 +0900

    ログインページの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,85 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
+import Background from "./components/Background";
+import Form from "./components/Form";
+import { useState } from "react";
+import TitleAndDescription from "./components/TitleAndDescription";
 
 export default function Home() {
+  const router = useRouter();
+  const [authFaildMessage, setAuthFaildMessage] = useState("");
+  const [buttonTitle, setButtonTitle] = useState("ログイン");
+  const inputFields = [
+    {
+      title: "メールアドレス",
+      type: "text",
+      name: "email",
+      id: "email",
+      value: "",
+      validation: {
+        required: "メールアドレスは必須入力です",
+        pattern: {
+          value: /^\S+[^\s@]+@[^\s@]+\.[^\s@]+$/,
+          message: "メールアドレスを正しく入力してください",
+        },
+      },
+    },
+    {
+      title: "パスワード",
+      type: "text",
+      name: "password",
+      id: "password",
+      value: "",
+      validation: {
+        required: "パスワードは必須入力です",
+        maxLength: {
+          value: 20,
+          message: "パスワードを２０文字以内で入力して下さい",
+        },
+      },
+    },
+  ];
+
+  const onSubmit = async (value: Record<string, string | number>) => {
+    setButtonTitle("少々お待ちください...");
+    const res = await fetch("/api/users/fetchUser", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(value),
+    });
+    const user = await res.json();
+    if (user.length > 0) {
+      const { user_id, daily_study_time } = user[0];
+      sessionStorage.setItem("userId", user_id);
+      sessionStorage.setItem("dailyStudyTime", daily_study_time);
+      router.push("/home");
+    } else {
+      setAuthFaildMessage("メールアドレス、またはパスワードが間違っています");
+      setButtonTitle("ログイン");
+    }
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+    <Background>
+      <TitleAndDescription
+        title={"ログイン"}
+        description={"アプリを利用するにはログインが必要です"}
+      >
+        <Form
+          message={authFaildMessage}
+          inputFields={inputFields}
+          onSubmit={onSubmit}
+          buttonTitle={buttonTitle}
+        >
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="mt-5 mx-auto text-blue-600 underline text-[15px] text-center"
+            onClick={() => {
+              router.push("/createUser");
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+            アカウントをお持ちでない方はこちら
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        </Form>
+      </TitleAndDescription>
+    </Background>
   );
 }
